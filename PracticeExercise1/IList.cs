03f70ee6d94d544a983dc48e5d0b0760c600c4d5
Kenan Casey <kenancasey@gmail.com>
2021-09-28 10:23:30 -0500

    Added code in class Tuesday

--- PracticeExercise1/IList.cs
+++ PracticeExercise1/IList.cs
@@ -3,5 +3,42 @@ namespace PracticeExercise1
 {
     public interface IList
     {
+
+        int Length { get; }
+
+        bool IsEmpty { get; }
+
+        // TODO
+        int First { get; }
+        // TODO
+        int Last { get; }
+
+        // Add i to end of list
+        void Append( int i );
+
+        // Add i to beginning of list
+        void Prepend(int i);
+
+        // Insert new value after first instance of exisiting value
+        // If existing value isn't found, then append to list.
+        void InsertAfter(int newValue, int existingValue);
+
+        // TODO
+        void InsertAt(int newValue, int index);
+        
+        int FirstIndexOf(int existingValue);
+
+        // TODO
+        void Remove(int value);
+
+        // TODO
+        void RemoveAt(int index);
+
+        // TODO
+        void Clear();
+
+        // TODO
+        IList Reverse();
+        
     }
 }
--- PracticeExercise1/Program.cs
+++ PracticeExercise1/Program.cs
@@ -6,7 +6,17 @@ namespace PracticeExercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList al = new ArrayList();
+
+            al.Append(1);
+            al.Append(2);
+            al.Append(3);
+            al.Append(5);
+            al.Append(8);
+
+            Console.WriteLine(al);
+
+            Console.ReadKey();
         }
     }
 }
